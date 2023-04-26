87109299cf8cf7361a9a071448bbb2b30ab26557
Steven Ward <sward3644@gmail.com>
2023-04-26 11:31:33 -0700

    Removed number scores

--- functionality.js
+++ functionality.js
@@ -3,36 +3,36 @@
 ===========================*/
 //#region Setup
 $(document).ready(function () {
-  makeHidden();
-  displayData();
-  setRandomColor();
+    makeHidden();
+    displayData();
+    setRandomColor();
 });
 
 function setRandomColor() {
-  let colorArray = [".tvshow"];
+    let colorArray = [".tvshow"];
 
-  for (let colorElement in colorArray) {
-    $(colorArray[colorElement]).each(function () {
-      $(this).css("background-color", random_color());
-    });
-  }
+    for (let colorElement in colorArray) {
+        $(colorArray[colorElement]).each(function () {
+            $(this).css("background-color", random_color());
+        });
+    }
 }
 
 function random_color() {
-  var letters = "0123456789ABCDEF".split("");
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.round(Math.random() * 15)];
-  }
-  color += "40";
-  return color;
+    var letters = "0123456789ABCDEF".split("");
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    color += "40";
+    return color;
 }
 
 // Hide everything then display something when dropdown is changed
 $("#dataSelection").on("change", function () {
-  event.preventDefault();
-  makeHidden();
-  displayData();
+    event.preventDefault();
+    makeHidden();
+    displayData();
 });
 
 //#endregion
@@ -41,31 +41,25 @@ $("#dataSelection").on("change", function () {
 
 // Hides all elements
 function makeHidden() {
-  $(".allTVshows").hide();
-  $(".genreBox").hide();
+    $(".allTVshows").hide();
+    $(".genreBox").hide();
 }
 
 // Shows the selected section and runs its function
 function displayData() {
-  switch ($("#dataSelection").val()) {
-    case "all":
-      allTVshows(1);
-      break;
-    case "rankedBtn":
-      allTVshows(2);
-      break;
-    case "pubDate":
-      allTVshows(3);
-      break;
-    case "episodeCount":
-      allTVshows(4);
-      break;
-    case "genre":
-      genreSetup("showGenre");
-      break;
-  }
+    switch ($("#dataSelection").val()) {
+        case "all":
+            allTVshows(1);
+            break;
+        case "pubDate":
+            allTVshows(3);
+            break;
+        case "genre":
+            genreSetup("showGenre");
+            break;
+    }
 
-  //setGradient();
+    //setGradient();
 }
 //#endregion
 
@@ -75,41 +69,32 @@ function displayData() {
 //#region All TVshows
 // Pulls all of the tvshows and displays them
 function allTVshows(sortingNum) {
-  let allTVshows = ``;
-  let tvshowsForAll = 0;
+    let allTVshows = ``;
+    let tvshowsForAll = 0;
 
-  $(".allTVshows").show();
+    $(".allTVshows").show();
 
-  let tvshowArr = JSON.parse(JSON.stringify(tvshowData));
+    let tvshowArr = JSON.parse(JSON.stringify(tvshowData));
 
-  if (sortingNum == 2) {
-    tvshowArr.sort(function (a, b) {
-      return b.rating - a.rating;
-    });
-  } else if (sortingNum == 3) {
-    tvshowArr.sort(function (a, b) {
-      return b.yearReleased - a.yearReleased;
-    });
-  } else if (sortingNum == 4) {
-    tvshowArr.sort(function (a, b) {
-      return b.episodeCount - a.episodeCount;
-    });
-  }
+    if (sortingNum == 3) {
+        tvshowArr.sort(function (a, b) {
+            return b.yearReleased - a.yearReleased;
+        });
+    }
 
-  for (let i = 0; i < tvshowArr.length; i++) {
-    allTVshows += `<div class="tvshow"><div class="title">${tvshowArr[i].title}</div><div class="year">${tvshowArr[i].yearReleased}</div>
-        <div class="year">${tvshowArr[i].episodeCount} episodes</div><div class="rating">Rating: ${tvshowArr[i].rating}/10</div>
-        <div class="rating">${tvshowArr[i].watchedAll == true ? "Completed" : "Watched 1+ Seasons"}</div></div>`;
-    tvshowsForAll += 1;
-    tvshowArr[i].id = i;
-  }
+    for (let i = 0; i < tvshowArr.length; i++) {
+        allTVshows += `<div class="tvshow"><div class="title">${tvshowArr[i].title}</div><div class="year">${tvshowArr[i].yearReleased}</div>
+        <div class="rating">${tvshowArr[i].rating}</div></div>`;
+        tvshowsForAll += 1;
+        tvshowArr[i].id = i;
+    }
 
-  $(".allTVshows").html(`     
+    $(".allTVshows").html(`     
               <h3>${tvshowsForAll} shows</h3>
               <div class="tvshowList">${allTVshows}</div>
       `);
 
-  setRandomColor();
+    setRandomColor();
 }
 //#endregion
 
@@ -118,55 +103,54 @@ function allTVshows(sortingNum) {
   ==========================================*/
 //#region Keyword
 function genreSetup(sortType) {
-  $(".genreBox").show();
-  let tvshowArr = JSON.parse(JSON.stringify(tvshowData));
-  let tvshowGenreArr = [];
+    $(".genreBox").show();
+    let tvshowArr = JSON.parse(JSON.stringify(tvshowData));
+    let tvshowGenreArr = [];
 
-  for (let i = 0; i < tvshowArr.length; i++) {
-    for (let j = 0; j < tvshowArr[i][sortType].length; j++) {
-      if (!tvshowGenreArr.includes(tvshowArr[i][sortType][j])) {
-        tvshowGenreArr.push(tvshowArr[i][sortType][j]);
-      }
+    for (let i = 0; i < tvshowArr.length; i++) {
+        for (let j = 0; j < tvshowArr[i][sortType].length; j++) {
+            if (!tvshowGenreArr.includes(tvshowArr[i][sortType][j])) {
+                tvshowGenreArr.push(tvshowArr[i][sortType][j]);
+            }
+        }
     }
-  }
-  tvshowGenreArr.sort();
-  printGenres(tvshowGenreArr, sortType);
+    tvshowGenreArr.sort();
+    printGenres(tvshowGenreArr, sortType);
 }
 
 function printGenres(tvshowGenreArr, sortType) {
-  let tvshowList = ``;
-  let tvshowCounts = {};
+    let tvshowList = ``;
+    let tvshowCounts = {};
 
-  for (let i = 0; i < tvshowGenreArr.length; i++) {
-    tvshowList += `<div class="genreTitle" id="${tvshowGenreArr[i].replace(/\s/g, "")}">${tvshowGenreArr[i]}<span class="countNum"></span></div>`;
-    for (let j = 0; j < tvshowData.length; j++) {
-      if (tvshowData[j][sortType] == undefined) {
-      } else {
-        if (tvshowData[j][sortType].includes(tvshowGenreArr[i]) == true) {
-          tvshowList += `<div class="tvshow"><div class="title">${tvshowData[j].title}</div><div class="year">${tvshowData[j].yearReleased}</div>
-              <div class="year">${tvshowData[j].episodeCount} episodes</div><div class="rating">Rating: ${tvshowData[j].rating}/10</div>
-              <div class="rating">${tvshowData[j].watchedAll == true ? "Completed" : "Watched 1+ Seasons"}</div></div>`;
-          tvshowCounts[tvshowGenreArr[i]] = tvshowCounts[tvshowGenreArr[i]] + 1 || 1;
+    for (let i = 0; i < tvshowGenreArr.length; i++) {
+        tvshowList += `<div class="genreTitle" id="${tvshowGenreArr[i].replace(/\s/g, "")}">${tvshowGenreArr[i]}<span class="countNum"></span></div>`;
+        for (let j = 0; j < tvshowData.length; j++) {
+            if (tvshowData[j][sortType] == undefined) {
+            } else {
+                if (tvshowData[j][sortType].includes(tvshowGenreArr[i]) == true) {
+                    tvshowList += `<div class="tvshow"><div class="title">${tvshowData[j].title}</div><div class="year">${tvshowData[j].yearReleased}</div>
+              <div class="rating">${tvshowData[j].rating}</div></div>`;
+                    tvshowCounts[tvshowGenreArr[i]] = tvshowCounts[tvshowGenreArr[i]] + 1 || 1;
+                }
+            }
         }
-      }
     }
-  }
 
-  $(".genreBox").html(`
+    $(".genreBox").html(`
               <div class="genreTVshows">
                   <div class="tvshowList">${tvshowList}</div>
               </div>
       `);
 
-  console.log(tvshowCounts);
+    console.log(tvshowCounts);
 
-  for (let i = 0; i < tvshowGenreArr.length; i++) {
-    console.log(tvshowCounts[tvshowGenreArr[i]]);
-    $(`#${tvshowGenreArr[i].replace(/\s/g, "")}`).html(`
+    for (let i = 0; i < tvshowGenreArr.length; i++) {
+        console.log(tvshowCounts[tvshowGenreArr[i]]);
+        $(`#${tvshowGenreArr[i].replace(/\s/g, "")}`).html(`
               ${tvshowGenreArr[i]}<span class="countNum">${tvshowCounts[tvshowGenreArr[i]]}</span>
           `);
-  }
+    }
 
-  setRandomColor();
+    setRandomColor();
 }
 //#endregion
